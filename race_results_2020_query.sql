6e30f14e76b2e548b110a89fa7f9db261f9ef08f
Naomi Mbugua <naomimbugua349@gmail.com>
2024-10-29 10:44:31 +0300

    solution to race results query

--- race_results_2020_query.sql
+++ race_results_2020_query.sql
@@ -1,3 +1,7 @@
+--1. Below is a database diagram
+-- Write a query that will display the results below (Note: some columns might be renamed
+-- but use the column names above). It should only show 2020 data and order by driver
+-- points.
 SELECT 
     races.race_year,
     races.race_name,
