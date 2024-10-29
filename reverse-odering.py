e56be13e64a4df4e7685a2930e866f102db0a51a
Naomi Mbugua <naomimbugua349@gmail.com>
2024-10-29 11:20:16 +0300

    added the questions as comments on top of the file

--- reverse-odering.py
+++ reverse-odering.py
@@ -1,3 +1,11 @@
+#4. Write a program that takes an integer as input and returns an integer with
+#reversed digit ordering.
+#Examples:
+#For input 500, the program should return 5.
+#For input -56, the program should return -65.
+#For input -90, the program should return -9.
+#For input 91, the program should return 19.
+
 def reverse_integer(num):
 
     reversed_num = 0
